6f5fb9849a43ed559d1bba60f027ce6d9eb8a1ad
shengbo wang <shuichen17@gmail.com>
2019-06-29 16:55:34 -0700

    update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-   res.send({hi: 'there'});
+   res.send('hi: there');
 })
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
