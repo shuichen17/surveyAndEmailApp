7e632ef742f23abd059355a9984a97aaae92b759
shengbo wang <shuichen17@gmail.com>
2019-06-29 16:52:41 -0700

    update

--- server/index.js
+++ server/index.js
@@ -3,11 +3,7 @@ const app = express();
 app.get('/', (req, res) => {
    res.send({hi: 'there'});
 })
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
